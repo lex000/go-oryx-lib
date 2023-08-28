04f56898341037b0adc7cda246474623655f4950
lex000 <87480405@qq.com>
2023-08-28 20:37:48 +0800

    增加avc解析示例

--- avc/example_test.go
+++ avc/example_test.go
@@ -20,3 +20,68 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package avc_test
+
+import (
+	"go-oryx-lib/avc"
+	"go-oryx-lib/flv"
+	"io"
+	"testing"
+)
+
+func TestAvcDecoderAndSample(t *testing.T) {
+	// To open a flv file, or http flv stream.
+	var r io.Reader
+	// r := io.("./h264.flv")
+	flvr, _ := flv.NewDemuxer(r)
+	tagType, tagSize, tagTS, err := flvr.ReadTagHeader()
+	if err != nil {
+		t.Errorf("read tag header failed, %v", err.Error())
+		return
+	}
+	t.Logf("tag type=%v, size=%v, ts=%v", tagType, tagSize, tagTS)
+
+	payload, err := flvr.ReadTag(tagSize)
+	if err != nil {
+		t.Errorf("read tag failed, %v", err.Error())
+		return
+	}
+
+	// To parse the flv tag.
+	vp, _ := flv.NewVideoPackager()
+	vf, err := vp.Decode(payload)
+	if err != nil {
+		t.Errorf("decode video failed, %v", err.Error())
+		return
+	}
+
+	var lengthSizeMinusOne uint8
+
+	if tagType == flv.TagTypeVideo {
+		if vf.Trait == flv.VideoFrameTraitSequenceHeader {
+			if vf.CodecID == flv.VideoCodecAVC {
+				avcCR := avc.NewAVCDecoderConfigurationRecord()
+				err := avcCR.UnmarshalBinary(vf.Raw)
+				if err != nil {
+					t.Errorf("AVCDecoderConfigurationRecord UnmarshalBinary failed, %v", err.Error())
+					return
+				} else {
+					lengthSizeMinusOne = avcCR.LengthSizeMinusOne
+				}
+			}
+		} else if vf.CodecID == flv.VideoCodecAVC {
+			avcSample := avc.NewAVCSample(lengthSizeMinusOne)
+			err := avcSample.UnmarshalBinary(vf.Raw)
+			if err != nil {
+				t.Errorf("avcSample UnmarshalBinary failed, %v", err.Error())
+			} else {
+				for i, nalu := range avcSample.NALUs {
+					if nalu.NALUType == avc.NALUTypeSEI {
+						t.Logf("avcSample UnmarshalBinary ok, nalu %v, NALRefIDC %v,  NALUType %v, Data %v", i, nalu.NALRefIDC, nalu.NALUType.String(), nalu.Data)
+					} else {
+						t.Logf("avcSample UnmarshalBinary ok, nalu %v, NALRefIDC %v,  NALUType %v, Data %v", i, nalu.NALRefIDC, nalu.NALUType.String(), len(nalu.Data))
+					}
+				}
+			}
+		}
+	}
+}
